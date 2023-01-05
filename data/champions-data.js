f5bd93e82cf01df4ae5cdcafff73ee2435053334
Mike Robles <mikej.robles@gmail.com>
2023-01-05 07:39:18 -0500

    adds league champions

---
+++ data/champions-data.js
@@ -0,0 +1,13 @@
+const champions = [
+  {champ: 'Vayne', role: 'ADC', quote: 'Hitting me is like boxing with shadows.'},
+  {champ: "Bel'Veth", role: 'Jungle', quote: 'Your memories, I want more.'},
+  {champ: 'Lillia', role: 'Jungle', quote: 'Dreams live on, even after the dreamer.'},
+  {champ: 'Jinx', role: 'ADC', quote: 'Rules are made to be broken. Like buildings! Or people!'},
+  {champ: 'Ahri', role: "Mid", quote: 'Tell me a secret'},
+  {champ: 'Rakan', role: "Support", quote: 'This is you getting slapped!'},
+  {champ: 'Garen', role: "Top", quote: 'FOR DEMACIA'}
+]
+
+export {
+  champions
+}
